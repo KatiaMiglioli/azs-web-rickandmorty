d94e2ee9d26bf2d55f2c0b9884260f134ea4a197
Kátia Miglioli <kt.miglioli@gmail.com>
2022-05-18 00:15:40 -0300

    Add: lista de episodios

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,32 +1,54 @@
 import * as React from 'react';
+
 import Card from '@mui/material/Card';
 import Grid from '@mui/material/Grid';
 import CardContent from '@mui/material/CardContent';
-import IconButton from '@mui/material/IconButton';
-import DeleteIcon from '@mui/icons-material/Delete';
-import FaceIcon from '@mui/icons-material/Face';
 import Chip from '@mui/material/Chip';
+import { Box } from '@mui/system';
+import Stack from '@mui/material/Stack';
 
-export default function CardBase(props) {
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import BookmarkBorderOutlinedIcon from '@mui/icons-material/BookmarkBorderOutlined';
+import StarIcon from '@mui/icons-material/Star';
+import PeopleAltOutlinedIcon from '@mui/icons-material/PeopleAltOutlined';
+
+import './style-components.css'
+
+const converterData_estreia = (data_) => {
+    const data = new Date(data_);
+    return data.toLocaleDateString("pt-BR");
+}
+
+
+
+export default function CardBase({ episodio }) {
     return (
-        <Card>
+        <Card className="card">
             <CardContent>
                 <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
                     <Grid item xs={6}>
-                        <span>Pilot</span>
-                        <span>#S01E01</span>
+                        <span className='card-titulo'>{episodio.name}</span>
                     </Grid>
                     <Grid item xs={6}>
-                        <IconButton aria-label="delete">
-                            <DeleteIcon />
-                        </IconButton>
-                        <IconButton aria-label="delete">
-                            <DeleteIcon />
-                        </IconButton>
+                        <Box sx={{
+                            display:"flex",
+                            alignItems: "center",
+                        justifyContent: "end"
+                        }}>
+                            <Chip variant="outlined" label={episodio.episode} />
+                            <span className='icon'>
+                                <FavoriteBorderOutlinedIcon color='error' />
+                            </span>
+                            <span className='icon'>
+                                <BookmarkBorderOutlinedIcon />
+                            </span>
+                        </Box>
                     </Grid>
                     <Grid item xs={12}>
-                        <Chip icon={<FaceIcon />} label="With Icon" />
-                        <Chip icon={<FaceIcon />} label="With Icon" />
+                        <Stack direction="row" spacing={1}>
+                            <Chip icon={<StarIcon />} label={converterData_estreia(episodio.air_date)} />
+                            <Chip icon={<PeopleAltOutlinedIcon />} label={episodio.characters.length} />
+                        </Stack>
                     </Grid>
                 </Grid>
             </CardContent>
--- src/pages/listaEpisodios.js
+++ src/pages/listaEpisodios.js
@@ -1,7 +1,39 @@
 import * as React from 'react';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+
+import { useQuery } from "@apollo/client";
+
+import { INFO_EPISODES } from "../querys/index";
+
+import Card from "../components/Card"
 
 export default function ListaEpisodios(props) {
-    return(
-        <div>LISTA DE EPISODIOS</div>
+    const { loading, error, data } = useQuery(INFO_EPISODES, { variables: { pagina: 1 } });
+
+    if (loading) {
+        return <p>Loading...</p>;
+    }
+
+    if (error) {
+        return <p>an error occurred...</p>;
+    }
+
+    return (
+        <Box sx={{ flexGrow: 1, display: 'flex', flexDirection: "column", alignItems: 'center',padding:"15px"}}>
+            <Grid container spacing={2}>
+                {data.episodes.results.map((ep, index) => (
+                    <Grid item xs={12} sm={6} md={4} lg={3} key={ep.name}>
+                        <Card episodio={ep}/>
+                    </Grid>
+                ))}
+            </Grid>
+            <Box sx={{padding:"30px"}}> 
+                <Button variant="outlined" size="large" color="primary">
+                    Ver mais
+                </Button>
+            </Box>
+        </Box>
     )
 }
